920479f5ce723bf8e9b1c880c51d9036bb5e97b4
Your Name <you@example.com>
2023-12-14 14:47:28 +0300

    week4

---
+++ week4/Program.cs
@@ -0,0 +1,14 @@
+﻿//for(int i = 0; i < 3; i++)
+//{
+//    Console.WriteLine($"{i + 1}. I will not skip Zoom classes.");
+//}
+
+Random rnd = new Random();
+
+int MyRandom;
+
+for (int i = 0; i < 5; i++)
+{
+    MyRandom = rnd.Next(1, 11);
+    Console.WriteLine($"{i}. {MyRandom}");
+}
---
+++ week42/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+Random rnd = new Random();
+
+int MyRandom = rnd.Next(1, 4);
+int UserG;
+
+bool LoopActive = true;
+
+int Atemps = 0;
+
+while (LoopActive && (Atemps < 3))
+{
+    Console.WriteLine("Try to guess the cpu number:");
+    UserG = int.Parse(Console.ReadLine());
+    Atemps++;
+
+    if (UserG == MyRandom)
+    {
+        Console.WriteLine("You won!");
+        LoopActive = false;
+    }
+}
+Console.WriteLine($"Number of tries: {Atemps}");
+Console.WriteLine("Have a nice day!");
+
